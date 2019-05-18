4ce4ce77336455adbd24cae49f0a0e02065076be
mukhtorov <mukhtorov_1994@mail.ru>
2019-05-18 13:17:53 +0900

    api fetch from express

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import "./stylesheets/App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome Back</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }))
+  }
+  render() {
+    return (
+      <div className="" >
+        <h1>Hello React </h1>
+        <ul>
+          {this.state.users.map(user =>
+            <li key={user.id}>{user.username}</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.json([
+    { id: 1, username: "sardorbek", status: "student" },
+    { id: 2, username: "Dilshodbek", status: "student" },
+    { id: 3, username: "Azizbek", status: "student" },
+    { id: 4, username: "Dilmurod", status: "student" },
+
+  ])
 });
 
 module.exports = router;
