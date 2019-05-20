7274f943caca0caffc2b91ce9d67a5d08e2e362d
mukhtorov <mukhtorov_1994@mail.ru>
2019-05-20 15:32:32 +0900

    flex implementation

--- frontend/src/components/layouts/footer.js
+++ frontend/src/components/layouts/footer.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import ReactDOM from 'react-dom';
 
-const Title = styled.h2`
-    font-size: 2.5em;
+const Title = styled.h4`
+    // font-size: 2.5em;
     text-align: center;
     color: red;
+    display: flex;
+    margin-right: 5px;
+    border: 2px solid palevioletred;
+
 
 `
 const Button = styled.button`
   background: ${props => props.primary ? "palevioletred" : "white"};
   color: ${props => props.primary ? "white" : "black"};
 
-  font-size: 1em;
+//   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
   border: 2px solid palevioletred;
   border-radius: 3px;
+  
 `;
 
 class Footer extends Component {
@@ -24,10 +29,12 @@ class Footer extends Component {
         return (
             <div>
                 <Title>
-                    Hello world
-            </Title>
-            <Button primary> Button</Button>
-            <Button > Button</Button>
+                    <div><h1>Sardorbek </h1> </div>
+                    <div><h1>Sardorbek </h1> </div>
+                    <div><h1>Sardorbek </h1> </div>
+                </Title>
+                <Button primary> Button</Button>
+                <Button > Button</Button>
             </div>
 
         );
