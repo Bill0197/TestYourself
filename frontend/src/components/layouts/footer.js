52eb5584bdd3c1ad4ebd368a63a277ca4909502e
mukhtorov <mukhtorov_1994@mail.ru>
2019-05-22 13:57:19 +0900

    ready footer

--- frontend/src/components/layouts/footer.js
+++ frontend/src/components/layouts/footer.js
@@ -9,6 +9,7 @@ import { Twitter } from 'styled-icons/fa-brands/Twitter'
 import { Telegram } from 'styled-icons/fa-brands/Telegram'
 import { Youtube } from 'styled-icons/fa-brands/Youtube'
 import { Linkedin } from 'styled-icons/fa-brands/Linkedin'
+import { Instagram } from 'styled-icons/fa-brands/Instagram'
 import '../../stylesheets/footer.css'
 
 
@@ -20,7 +21,7 @@ const Container = styled.div`
 // four div
 const ContElement = styled.div`
 background: black;
-height: 500px;
+height: 450px;
 color: white;
 justify-content: center;
 display: flex;
@@ -31,12 +32,26 @@ justify-content: space-around;
     font-size: 1.3em;
 }
 `
-
+// image logo
 const logo = require('../../images/logo-png.png');
 
+///  SocilaLinkWrapper
+const SocilaLinkWrapper = styled.div`
+    color: #f6db90;
+    font-size: 1.1em;
+    justify-items: center;
+
+
+    a{
+        color: #f6db90;
+        padding: 5px;
+    }
+`
+
+// buttons
 const Button = styled.button`
 display:flex;
-border: 2px solid white;
+border: 2px solid #f6db90;
 background: black;
 border-radius: 10px;
 justify-content: center;
@@ -44,18 +59,22 @@ color: white;
 align-items:center;
 cursor:pointer;
 `
+
+// download button decloration
 const Download = styled.div`
 display: flex;
 justify-content: flex-start ;
 color: white;
 justify-content: center;
-
 `
+
+// apple icon
 const Apple = styled(AppStoreIos)`
 height: 3em;
 width: 50px;
 color: white;
-`;
+`
+//google icon
 const Play = styled(PlayStore)`
 height: 3em;
 width: 50px;
@@ -63,46 +82,66 @@ color: white;
 justify-content: flex-end;
 `;
 
+//facebook icon
 const Fb = styled(Facebook)`
-color: white;
+color: #f6db90;
 margin-right: 5px;
 height: 20px;
 width: 30px;
+padding: 5px;
+
 `
 
 const Tg = styled(Telegram)`
-color: white;
+color: #f6db90;
 margin-right: 5px;
 height: 20px;
 width: 30px;
-`
+padding: 5px;
 
+`
+//git icon
 const Git = styled(Github)`
-color: white;
+color: #f6db90;
 margin-right: 5px;
 height: 20px;
 width: 30px;
+padding: 5px;
 `
 
+//twitter icon
 const Tw = styled(Twitter)`
-color: white;
+color: #f6db90;
 margin-right: 5px;
 height: 20px;
 width: 30px;
+padding: 5px;
+
 `
 
 const Yt = styled(Youtube)`
-color: white;
+color: #f6db90;
 margin-right: 5px;
 height: 20px;
 width: 30px;
-`
+padding: 5px;
 
+`
+//Linkedin icon
 const In = styled(Linkedin)`
-color: white;
 margin-right: 5px;
 height: 20px;
 width: 30px;
+padding: 5px;
+
+`
+//Instagram icon
+const Ig = styled(Instagram)`
+margin-right: 5px;
+height: 20px;
+width: 30px;
+padding: 5px;
+
 `
 
 class Footer extends Component {
@@ -118,7 +157,9 @@ class Footer extends Component {
                             <Download>
                                 <Button > <Apple /><span>Apple Store</span></Button>
                                 <Button> <Play />Google Play </Button>
-                            </Download>
+                            </Download> <br /> <br /> <br />
+
+                            <a style={{ color: "#f6db90" }} href="mailto:sardor.sejong@gmail.com">sardor.sejong@gmail.com</a>
 
                         </section>
                     </div>
@@ -138,45 +179,60 @@ class Footer extends Component {
                     </div>
                     <div className="container3">
                         <br /> <br /> <br /> <br />
-                        Connect
+                        <b>Content</b>
                         <br /> <br />
                         {/* social links */}
+                        <SocilaLinkWrapper>
+                            {/* Facebook */}
+                            <div style={{ display: "flex" }}>
+                                <br /> <Fb className="socialLink" />
+                                <span> <a href="http://facebook.com/profile.php?id=100004454506696"> Facebook </a></span>
+                            </div>
+                            {/* Telegram */}
+                            <div style={{ display: "flex" }}>
+                                <br />  <Tg className="socialLink" />
+                                <span> <a href="http://t.me/smd_94">Telegram</a> </span>
+                            </div>
+                            {/* GitHub */}
+                            <div style={{ display: "flex" }}>
+                                <br />   <Git className="socialLink" />
+                                <span><a href="https://github.com/mukhtorov"> GitHub </a></span>
+                            </div>
 
-                        {/* Facebook */}
-                        <div style={{ display: "flex" }}>
-                            <br /> <Fb className="socialLink" />
-                            <span>Facebook</span>
-                        </div>
-                        {/* Telegram */}
-                        <div style={{ display: "flex" }}>
-                            <br />  <Tg className="socialLink" />
-                            <span>Telegram</span>
-                        </div>
-                        {/* GitHub */}
-                        <div style={{ display: "flex", color: "white" }}>
-                            <br />  <Git className="socialLink" />
-                            <span>GitHub</span>
-                        </div>
+                            {/* Twitter */}
+                            <div style={{ display: "flex" }}>
+                                <br />  <Tw className="socialLink" />
+                                <span > <a href="http://twitte.com/smd_94">Twitter </a></span>
+                            </div>
 
-                        {/* Twitter */}
-                        <div style={{ display: "flex" }}>
-                            <br />  <Tw className="socialLink" />
-                            <span>Twitter</span>
-                        </div>
-
-                        {/* youtube */}
-                        <div style={{ display: "flex" }}>
-                            <br />  <Yt className="socialLink" />
-                            <span>Youtube</span>
-                        </div>
-                        {/* Linkedin */}
-                        <div style={{ display: "flex" }}>
-                            <br />  <In className="socialLink" />
-                            <span>Linkedin</span>
-                        </div>
-
+                            {/* youtube */}
+                            <div style={{ display: "flex" }}>
+                                <br />   <Yt className="socialLink" />
+                                <span><a href="https://www.youtube.com/channel/UCIIcraLARkWo9wc5c699vzA?view_as=subscriber"> Youtube </a></span>
+                            </div>
+                            {/* Linkedin */}
+                            <div style={{ display: "flex" }}>
+                                <br />   <In className="socialLink" />
+                                <span><a href="http://linkedin.com/in/smd94"> Linkedin </a></span>
+                            </div>
+                            {/* Instagram */}
+                            <div style={{ display: "flex" }}>
+                                <br />  <Ig className="socialLink" />
+                                <span> <a href="http://instagram.com/smd_94"> Instagram </a></span>
+                            </div>
+                        </SocilaLinkWrapper>
                     </div>
-                    <div className="container4">Container4</div>
+                    <div className="container4">
+                        <br /> <br /> <br /> <br />
+                        <b>want to know more?</b>
+                        <br /> <br /> <br /> <br />
+
+                        <span>
+                            Create Your Future <br />
+                            Your future is on your hand <br />
+                            We are the solution!
+                        </span>
+                    </div>
                 </ContElement>
             </Container>
 
